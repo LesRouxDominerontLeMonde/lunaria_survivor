e44c59a1b900cdea9a7ef5bdc85d28ce44c44763
LesRouxDominerontLeMonde <105645582+LesRouxDominerontLeMonde@users.noreply.github.com>
2023-11-06 13:38:12 +0000

    succes tween movement fusion
    beter code for beter reading =)

--- scenes/objects/stones/medium_stones.gd
+++ scenes/objects/stones/medium_stones.gd
@@ -4,7 +4,7 @@ extends Area2D
 
 
 func tween_collect_movement(percent: float, start_position: Vector2):
-	global_position = start_position.lerp(Globals.player_pos, percent)
+	global_position = start_position.lerp(Globals.player_pos, percent) 
 
 
 func collected():
@@ -16,22 +16,23 @@ func disable_collision():
 	collision_shape_2d.disabled = true
 
 
-#Signal conected _on_area_entered (medium_stone is type area 2D)
+	#Signal conected _on_area_entered (medium_stone is type area 2D)
 func _on_area_entered(_other_area):
 	Callable(disable_collision).call_deferred()
 	
-	var tween_movement = create_tween()
-	var tween_size = create_tween()
+	var tween = create_tween()
+	tween.set_parallel()
 		
-	# stone move back than to the player
-	tween_movement.tween_method(tween_collect_movement.bind(global_position), 0.0, 1.0, 0.8)\
-	.set_ease(Tween.EASE_IN)\
-	.set_trans(Tween.TRANS_BACK)
-	# stone upscale a litle than descale back
-	tween_size.tween_property($stone_sprite, "scale", Vector2(0.15, 0.15) , 0.4)
-	tween_size.tween_property($stone_sprite, "scale", Vector2(0.08, 0.08) , 0.2)
-	print($stone_sprite.scale)
-	
-
-	
-	tween_movement.tween_callback(collected)
+		# stone movement
+	tween.tween_method(tween_collect_movement.bind(global_position), 0.0, 1.0, 0.8)\
+		.set_ease(Tween.EASE_IN)\
+		.set_trans(Tween.TRANS_BACK)
+		# stone upscale and descale back
+	tween.tween_property($stone_sprite, "scale", Vector2(0.15, 0.15) , 0.4)
+	tween.tween_property($stone_sprite, "scale", Vector2.ZERO , 0.35)\
+		.set_delay(0.4)\
+		.from(Vector2(0.15, 0.15))\
+		.set_ease(Tween.EASE_IN)\
+		.set_trans(Tween.TRANS_CUBIC)
+	tween.chain()
+	tween.tween_callback(collected)
