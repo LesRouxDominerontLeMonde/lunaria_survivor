c0ba240c2ef74d395d46c5ab5d07bef60dbc035c
LesRouxDominerontLeMonde <105645582+LesRouxDominerontLeMonde@users.noreply.github.com>
2023-11-06 13:25:02 +0000

    stone exp movement added
    until i add tween_movement and tween_scale separatly, will try to fuse them next

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -1,13 +1,17 @@
 extends Node
 
-const SPAWN_RADIUS = 330 # display/window/size/viewport_width : 640.. 640/2 +10
+const SPAWN_RADIUS = 360 # display/window/size/viewport_width : 640.. 640/2 +10 =330 (I augmented)
 
 @export var slime_scene: PackedScene
+@export var lizard_scene: PackedScene
 @export var arena_time_manager: Node
 
 var base_spawn_time = 0
+var total_enemies = 0
+var enemy_table = WeightedTable.new()
 
 func _ready():
+	enemy_table.add_item(slime_scene, 10)
 	base_spawn_time = $Timer.wait_time
 	$Timer.timeout.connect(on_timer_timeout)
 	arena_time_manager.arena_dificulty_increased.connect(on_arena_dificulty_increased)
@@ -20,7 +24,11 @@ func on_timer_timeout():
 	var spawn_position = Globals.player_pos + (random_direction * SPAWN_RADIUS)
 	
 	# Instantiate and give the spawn pos
-	var enemy = slime_scene.instantiate() as Node2D
+	var enemy_picked = enemy_table.pick_item()
+	print(enemy_picked)
+	var enemy = enemy_picked.instantiate() as Node2D
+	total_enemies += 1
+	print(total_enemies)
 	
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
 	entities_layer.add_child(enemy)
@@ -28,9 +36,11 @@ func on_timer_timeout():
 
 
 func on_arena_dificulty_increased(arena_dificulty: int):
-	# /12 car 12 fois 5 seconde dans 1 minutes
+	# /12 car 60/12 = Every 5 secondes arena dificulty increase
 	var time_off = (.1 / 12) * arena_dificulty
 	time_off = min(time_off, .7) #Peut-etre changer, 0.7 pour pas descendre en dessous de 0.3s de spawn
-	print("time off:",time_off)
 	$Timer.wait_time = base_spawn_time - time_off
-	print("wait_time:", $Timer.wait_time)
+	
+	if arena_dificulty == 2:
+		enemy_table.add_item(lizard_scene, 20)
+	
--- scenes/objects/stones/medium_stones.gd
+++ scenes/objects/stones/medium_stones.gd
@@ -1,7 +1,37 @@
 extends Area2D
 
+@onready var collision_shape_2d = $CollisionShape2D
 
-func _on_area_entered(_other_area):
-	# add the stone via the function add_stone on the player, AND destroy the stone after
-	Globals.emit_experience_colected(10)
+
+func tween_collect_movement(percent: float, start_position: Vector2):
+	global_position = start_position.lerp(Globals.player_pos, percent)
+
+
+func collected():
+	Globals.emit_experience_colected(0)
 	queue_free()
+
+
+func disable_collision():
+	collision_shape_2d.disabled = true
+
+
+#Signal conected _on_area_entered (medium_stone is type area 2D)
+func _on_area_entered(_other_area):
+	Callable(disable_collision).call_deferred()
+	
+	var tween_movement = create_tween()
+	var tween_size = create_tween()
+		
+	# stone move back than to the player
+	tween_movement.tween_method(tween_collect_movement.bind(global_position), 0.0, 1.0, 0.8)\
+	.set_ease(Tween.EASE_IN)\
+	.set_trans(Tween.TRANS_BACK)
+	# stone upscale a litle than descale back
+	tween_size.tween_property($stone_sprite, "scale", Vector2(0.15, 0.15) , 0.4)
+	tween_size.tween_property($stone_sprite, "scale", Vector2(0.08, 0.08) , 0.2)
+	print($stone_sprite.scale)
+	
+
+	
+	tween_movement.tween_callback(collected)
